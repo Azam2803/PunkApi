a16e8373efc4b46d4dcba0f642fc6ce9c50f4057
Azam <mohdazamnoor28@gmail.com>
2022-01-04 19:22:12 +0530

    adding page total and pagination

--- punkapi/index.js
+++ punkapi/index.js
@@ -1,39 +1,44 @@
+Vue.component('v-pagination', window['vue-plain-pagination'])
+
 const app = new Vue({
     el:'#app1',
     data:{
         page:'1',
         perPage:'5',
-        baseUrl:'https://api.punkapi.com/v2/beers?page=1&per_page=5',
-        totalItems: '325',
-        totalPages: '65',
-        nextPage:'2',
-        prevPage:'0',
-        firstPage:'1',
-        lastPage:'65',
-        currentPage:'1',
+        // baseUrl:'https://api.punkapi.com/v2/beers?page=1&per_page=5',
+        baseUrl:'https://api.punkapi.com/v2/beers?',
+        totalItems: 325,
+        totalPages: 65,
+        lastPage: 65,
         result: '',
         searchItem:'',
         pageBtn:'',
+        searchTotalItem:'',
+        totalCount: 0,
+        data: [],
+        prevCurrentPage: 1,
+        currentPage: 1,
+        total: 9,
+        bootstrapPaginationClasses: {
+            ul: 'pagination',
+            li: 'page-item',
+            liActive: 'active',
+            liDisable: 'disabled',
+            button: 'page-link'
+        },
+        paginationAnchorTexts: {
+            first: 'First',
+            prev: 'Previous',
+            next: 'Next',
+            last: 'Last'
+        }
     },
     methods:{
         fetchBeerData: function () {
-            this.baseUrl=`https://api.punkapi.com/v2/beers?page=${this.page}&per_page=${this.perPage}`
-            fetch(this.baseUrl)
+            // this.baseUrl=`https://api.punkapi.com/v2/beers?page=${this.page}&per_page=${this.perPage}`
+            fetch(this.baseUrl+`page=${this.page}&per_page=${this.perPage}`)
             .then(response => response.json())
             .then(data => this.result=data)
-
-            // fetch(`https://api.punkapi.com/v2/beers?page=65&per_page=5`)
-            // fetch(`https://api.punkapi.com/v2/beers?page=${this.page}&per_page=${this.perPage}`)
-            // .then(response => response.json())
-            // .then(data => this.result=data)
-
-            // axios.get(this.baseUrl+'posts')
-            // .then(response => {
-            // this.data = response.data;
-            // })
-            // .catch(response => {
-            // console.log(response);
-            // });
           },
           selectItemBtn: function() {
             let e = document.getElementById("select-items");
@@ -43,39 +48,20 @@ const app = new Vue({
           setPages: function () {
             this.totalPages = Math.ceil(this.totalItems / this.perPage);
             this.lastPage = this.totalPages;
-            // for (let index = 1; index <= numberOfPages; index++) {
-            //   this.pages.push(index);
-            // }
-          },
-          nextPageBtn:  function(){
-              if(this.nextPage<=this.lastPage){
-                  this.page=this.nextPage;
-                  this.currentPage=this.page;
-                  this.nextPage=parseInt(this.nextPage)+1;
-                  this.prevPage=parseInt(this.prevPage)+1;
-              }
-              console.log('next Button clicked');
-          },
-          prevPageBtn: function(){
-              if(this.prevPage>=1){
-                  this.page=this.prevPage;
-                  this.currentPage=this.page;
-                  this.prevPage=parseInt(this.prevPage)-1;
-                  this.nextPage=parseInt(this.nextPage)-1;
-              }
-              console.log('previous Button clicked');
           },
           filterItem: function(){
             if(this.searchItem){
-                fetch(`https://api.punkapi.com/v2/beers?beer_name=${this.searchItem}`)
+                fetch(this.baseUrl+`beer_name=${this.searchItem}`)
                 .then(response => response.json())
-                .then(data=>this.result=data)
+                .then(data=>{
+                    this.searchTotalItem = data.length;
+                    this.result=data;
+                })
             }
           },
           listItemBtn: function(){
             if(this.pageBtn){
-                this.baseUrl=`https://api.punkapi.com/v2/beers?page=${this.pageBtn}&per_page=${this.perPage}`
-                fetch(this.baseUrl)
+                fetch(this.baseUrl+`page=${this.pageBtn}&per_page=${this.perPage}`)
                 .then(response => response.json())
                 .then(data=>this.result=data)
 
@@ -83,31 +69,40 @@ const app = new Vue({
                 this.nextPage=parseInt(this.pageBtn)+1;
                 this.currentPage=this.pageBtn;
             }
-          }
+          },
+        totalItemCount: function(newPageCount=1) {
+
+            this.data = fetch(`https://api.punkapi.com/v2/beers?page=${newPageCount}&per_page=80`)
+                .then(response  =>  response.json())
+                .then(data => {
+                    return data
+                })
+
+            this.data.then((data)=>{
+                if(data.length){
+                    newPageCount += 1;
+                    this.totalCount = parseInt(this.totalCount)+parseInt(data.length);
+                    this.totalItemCount(newPageCount);
+
+                }
+            })
+
+        }
     },
-    // computed:{
-    //     filterItem: function(){
-    //         if(this.searchItem){
-    //             this.searchBaseUrl=`https://api.punkapi.com/v2/beers?beer_name=${this.searchItem}`
-    //             fetch(searchBaseUrl)
-    //             .then(response => response.json())
-    //             .then(data=>this.result=data)
-    //         }
-    //     }
-    // },
     created() {
-        this.fetchBeerData()
+        this.totalItemCount();
+        this.fetchBeerData();
     },
     watch: {
         perPage () {
           this.setPages();
         },
-        nextPage (){
-            this.fetchBeerData()
-        },
-        prevPage (){
+        currentPage () {
+            this.page = this.currentPage;
             this.fetchBeerData()
         }
-    } 
+    }
 })
+
+
 
